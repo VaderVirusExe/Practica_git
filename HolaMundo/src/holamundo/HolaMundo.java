2ab6c5d6a122bae4d90da1c308aa9a6fcf4deb36
dam <dam@203P01>
2019-01-15 10:05:41 +0100

    Versión 2.0 ultimo paso

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -17,6 +17,8 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Práctica GitHub paso 3");
         System.out.println("Práctica GitHub paso 5");
+        System.out.println("Ahora creo una rama llamada rama24");
+        System.out.println("Cambios antes de unir la rama24 con la rama master");
     }
     
 }
